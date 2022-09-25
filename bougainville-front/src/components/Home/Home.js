a09629971931228af7e7bc9a2f9cc1ebf306c090
bougainville-studio <bougainvillestudio@gmail.com>
2022-09-25 21:23:11 +0000

    changes on Server

--- bougainville-front/src/components/Home/Home.js
+++ bougainville-front/src/components/Home/Home.js
@@ -36,7 +36,7 @@ export default function Home() {
                     </Container>
                     <ShopCase />
                     <Footer /> 
-                </div>)
+                </div>
             </>
     )
 }
